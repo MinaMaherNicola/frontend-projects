23f8d842d6e3584b632effde8287f057b2b50724
MinaMaherNicola <minamahernicola@outlook.com>
2021-01-14 19:30:24 +0200

    Last step in functionality: impelement a loop to update time

--- launch-countdown-timer-main/app/js/app.js
+++ launch-countdown-timer-main/app/js/app.js
@@ -1,53 +1,53 @@
 const launchTime = new Date('Jan 20, 2021 23:15:30');
 
-let timeInDays = Math.abs(
-  (launchTime.getTime() - Date.now()) / 1000 / 60 / 60 / 24
-);
-let timeInHours = Math.abs((Math.floor(timeInDays) - timeInDays) * 24);
-let timeInMinutes = Math.abs((Math.floor(timeInHours) - timeInHours) * 60);
-let timeInSeconds = Math.abs((Math.floor(timeInMinutes) - timeInMinutes) * 60);
-
-timeInDays = Math.floor(timeInDays);
-timeInHours = Math.floor(timeInHours);
-timeInMinutes = Math.floor(timeInMinutes);
-timeInSeconds = Math.floor(timeInSeconds);
-
 const days = document.querySelector('.counter__card-duaration-days');
 const hours = document.querySelector('.counter__card-duaration-hours');
 const minutes = document.querySelector('.counter__card-duaration-minutes');
 const seconds = document.querySelector('.counter__card-duaration-seconds');
 
 const specifyTime = (T) => {
+  let timers = [];
+  let timeInDays = Math.abs((T.getTime() - Date.now()) / 1000 / 60 / 60 / 24);
+  let timeInHours = Math.abs((Math.floor(timeInDays) - timeInDays) * 24);
+  let timeInMinutes = Math.abs((Math.floor(timeInHours) - timeInHours) * 60);
+  let timeInSeconds = Math.abs(
+    (Math.floor(timeInMinutes) - timeInMinutes) * 60
+  );
 
-}
+  timeInDays = Math.floor(timeInDays);
+  timeInHours = Math.floor(timeInHours);
+  timeInMinutes = Math.floor(timeInMinutes);
+  timeInSeconds = Math.floor(timeInSeconds);
+  timers.push(timeInDays);
+  timers.push(timeInHours);
+  timers.push(timeInMinutes);
+  timers.push(timeInSeconds);
+  return timers;
+};
 
-const updateTime = (d, h, m, s) => {
-  if (d % 10 === d) {
-    days.textContent = '0' + d.toString();
+const updateTime = (timers) => {
+  if (timers[0] % 10 === timers[0]) {
+    days.textContent = '0' + timers[0].toString();
   } else {
-    days.textContent = d.toString();
+    days.textContent = timers[0].toString();
   }
-  if (h % 10 === h) {
-    hours.textContent = '0' + h.toString();
+  if (timers[1] % 10 === timers[1]) {
+    hours.textContent = '0' + timers[1].toString();
   } else {
-    hours.textContent = h.toString();
+    hours.textContent = timers[1].toString();
   }
-  if (m % 10 === m) {
-    minutes.textContent = '0' + m.toString();
+  if (timers[2] % 10 === timers[2]) {
+    minutes.textContent = '0' + timers[2].toString();
   } else {
-    minutes.textContent = m.toString();
+    minutes.textContent = timers[2].toString();
   }
-  if (s % 10 === s) {
-    seconds.textContent = '0' + s.toString();
+  if (timers[3] % 10 === timers[3]) {
+    seconds.textContent = '0' + timers[3].toString();
   } else {
-    seconds.textContent = s.toString();
+    seconds.textContent = timers[3].toString();
   }
 };
 
-updateTime(timeInDays, timeInHours, timeInMinutes, timeInSeconds);
-
-while (true) {
-  setTimeout(() => {
-    updateTime(timeInDays, timeInHours, timeInMinutes, timeInSeconds);
-  }, 1000);
-}
+setTimeout(() => {
+  updateTime(specifyTime(launchTime));
+}, 1000);
